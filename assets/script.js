1bfd718cf3e5045a94a8067da1e8bd3be338d4bc
Taleh Rzayev <taleh.rzayev.i@asoiu.edu.az>
2023-03-13 18:14:54 +0400

    version v1.0

---
+++ assets/script.js
@@ -0,0 +1,62 @@
+var htmlEditor = CodeMirror(document.getElementById("code-container-html"), {
+  mode: "htmlmixed",
+  lineNumbers: true,
+  mode: "css",
+  theme: "dracula",
+  fullscreen: true,
+});
+var cssEditor = CodeMirror(document.getElementById("code-container-css"), {
+  mode: "css",
+  lineNumbers: true,
+  mode: "css",
+  theme: "dracula",
+  fullscreen: true,
+});
+
+
+function generateImage() {
+  const imageUrl = document.getElementById('image-url').value;
+  const imageWidth = document.getElementById('image-width').value;
+  const imageHeight = document.getElementById('image-height').value;
+
+  const imageHTML = `
+  <div class="box">
+    <div class="box-resize">
+      <img src="${imageUrl}" class="resposive-img" width="${imageWidth}" height="${imageHeight}" />
+    </div>
+  </div>`;
+
+  document.getElementById('image-container').innerHTML = imageHTML;
+
+  const codeHTML = `
+<div class="box">
+  <div class="box-resize">
+    <img src="${imageUrl}" class="resposive-img" width="${imageWidth}" height="${imageHeight}" />
+  </div>
+</div>
+  `;
+
+  const codeCSS = `
+.box {
+  position: relative;
+  width: 100%;
+  top: 0;
+  left: 0;
+  padding: ${(imageHeight / imageWidth) * 100}% 0 0 0;
+}
+.box-resize {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+}
+.responsive-img {
+  min-width: 100%;
+  min-height: 100%;
+}`;
+
+  htmlEditor.setValue(codeHTML);
+  cssEditor.setValue(codeCSS);
+}
